70026fd219675a0723554b46b21accaaa989f217
Josh Eldridge <jeldridge@michiganlabs.com>
2022-04-17 23:29:05 -0400

    Swipeable details

--- app/src/main/java/com/matrix/materializedsmite/ui/GodDetailsExperiment.kt
+++ app/src/main/java/com/matrix/materializedsmite/ui/GodDetailsExperiment.kt
@@ -2,23 +2,26 @@ package com.matrix.materializedsmite.ui
 
 import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.FractionalThreshold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.rememberSwipeableState
 import androidx.compose.material.swipeable
-import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.matrix.materializedsmite.viewmodels.SmiteViewModel
@@ -28,25 +31,73 @@ import com.matrix.materializedsmite.viewmodels.SmiteViewModel
 fun GodDetailsExperiment(smiteAppViewModel: SmiteViewModel, modifier: Modifier = Modifier) {
   val selectedGod = smiteAppViewModel.selectedGod.value
   val swipeState = rememberSwipeableState(initialValue = 0)
-  val anchors = mapOf(0f to 0, -LocalConfiguration.current.screenHeightDp.toFloat() - 1f to 1)
+  val heightInPx: Float = with(LocalDensity.current) {
+    LocalConfiguration.current.screenHeightDp.dp.toPx()
+  }
+  val heightInDp: Dp = LocalConfiguration.current.screenHeightDp.dp
+  val anchors = mapOf(0f to 0, -heightInPx - 1f to 1)
   Log.d("SCREEN HEIGHT: ", LocalConfiguration.current.screenHeightDp.toString())
-  Log.d("SWIPE STATE: ", swipeState.offset.value.dp.toString())
+  Log.d("SWIPE STATE: ", with(LocalDensity.current) { swipeState.offset.value.toDp() }.toString())
   if (selectedGod != null) {
-    Image(
-      painter = rememberImagePainter(selectedGod.godCardURL),
-      contentDescription = selectedGod.name,
-      contentScale = ContentScale.Crop,
-      alignment = Alignment.TopCenter,
-      modifier = Modifier
-        .fillMaxSize()
-        .swipeable(
-          state = swipeState,
-          anchors = anchors,
-          thresholds = { _, _ -> FractionalThreshold(0.3f) },
-          orientation = Orientation.Vertical
+    Box(
+      contentAlignment = Alignment.BottomCenter,
+      modifier = Modifier.fillMaxSize(),
+    ) {
+      Image(
+        painter = rememberImagePainter(selectedGod.godCardURL),
+        contentDescription = selectedGod.name,
+        contentScale = ContentScale.Crop,
+        alignment = Alignment.TopCenter,
+        modifier = Modifier
+          .matchParentSize()
+          .swipeable(
+            state = swipeState,
+            anchors = anchors,
+            thresholds = { _, _ -> FractionalThreshold(0.3f) },
+            orientation = Orientation.Vertical
+          )
+          .offset(y = with(LocalDensity.current) { (swipeState.offset.value * .10f).toDp() })
+      )
+      Box(
+        modifier = Modifier
+          .fillMaxSize()
+          .background(
+            brush = Brush.verticalGradient(
+              0F to Color.Transparent,
+              .5F to Color(0x40000000),
+              .75f to Color(0x80000000),
+              1f to Color(0xFF000000)
+            )
+          )
+//          .border(1.dp, Color.Red)
+//          .height(heightInDp + with(LocalDensity.current) { swipeState.offset.value.toDp() })
+      )
+      Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+          .padding(start = 16.dp, end = 16.dp, bottom = 64.dp)
+          .offset(y = with(LocalDensity.current) { (swipeState.offset.value * .10f).toDp() })
+      ) {
+        Icon(
+          imageVector = Icons.Default.KeyboardArrowUp,
+          contentDescription = "Drag Up",
+          modifier = Modifier.size(32.dp)
         )
-        .offset(y = swipeState.offset.value.dp)
-    )
+        Text(text = selectedGod.name, fontWeight = FontWeight.Bold, style = MaterialTheme.typography.titleLarge)
+        Text(text = selectedGod.title, fontWeight = FontWeight.Bold, style = MaterialTheme.typography.titleMedium)
+      }
+      val detailsOffset: Dp =
+        if (swipeState.currentValue == 0) heightInDp
+        else with(LocalDensity.current) { (-swipeState.offset.value).toDp() }
+      Card(modifier = Modifier
+        .fillMaxWidth()
+        .height(heightInDp)
+        .offset(y = detailsOffset)
+      ) {
+        Text("test")
+      }
+    }
+
   }
 }
 
