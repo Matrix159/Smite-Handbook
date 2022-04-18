09636921a93d811db4b13109dd4a19a90eab7339
Josh Eldridge <jeldridge@michiganlabs.com>
2022-04-17 21:05:55 -0400

    adding navigation animations

--- app/src/main/java/com/matrix/materializedsmite/SmiteApp.kt
+++ app/src/main/java/com/matrix/materializedsmite/SmiteApp.kt
@@ -1,15 +1,14 @@
 package com.matrix.materializedsmite
 
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
-import com.matrix.materializedsmite.ui.GodDetails
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.matrix.materializedsmite.ui.GodDetailsExperiment
 import com.matrix.materializedsmite.ui.GodList
 import com.matrix.materializedsmite.viewmodels.SmiteViewModel
@@ -20,13 +19,14 @@ object NavigationRoutes {
   val GotDetails = "GodDetails"
 }
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun SmiteApp() {
-  val navController: NavHostController = rememberNavController()
+  val navController: NavHostController = rememberAnimatedNavController()
   val navBackStackEntry by navController.currentBackStackEntryAsState()
 
   val smiteViewModel: SmiteViewModel = viewModel()
-  NavHost(navController = navController, startDestination = NavigationRoutes.GodList) {
+  AnimatedNavHost(navController = navController, startDestination = NavigationRoutes.GodList) {
     composable(NavigationRoutes.GodList) {
       //val smiteViewModel = hiltViewModel<SmiteViewModel>()
       GodList(smiteViewModel) { selectedGod ->
--- app/src/main/java/com/matrix/materializedsmite/ui/GodList.kt
+++ app/src/main/java/com/matrix/materializedsmite/ui/GodList.kt
@@ -2,18 +2,14 @@ package com.matrix.materializedsmite.ui
 
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.GridCells
-import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.LazyVerticalGrid
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -21,7 +17,6 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberImagePainter
@@ -44,7 +39,7 @@ fun GodList(
 
     if (gods.isNotEmpty()) {
       LazyVerticalGrid(
-        cells = GridCells.Fixed(3),
+        columns = GridCells.Fixed(3),
         modifier = Modifier.fillMaxSize()
       ) {
         items(items = gods) { god ->
