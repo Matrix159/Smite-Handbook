ffcdca671ff47abe671c9b75a883b8281058b82d
Josh Eldridge <jeldridge@michiganlabs.com>
2022-11-25 18:10:49 -0500

    Change gradle to 7.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,8 +28,8 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application").version("8.0.0-alpha08").apply(false)
-    id("com.android.library").version("8.0.0-alpha08").apply(false)
+    id("com.android.application").version("7.3.1").apply(false)
+    id("com.android.library").version("7.3.1").apply(false)
     kotlin("android").version("1.7.21").apply(false)
     kotlin("multiplatform").version("1.7.21").apply(false)
 }
