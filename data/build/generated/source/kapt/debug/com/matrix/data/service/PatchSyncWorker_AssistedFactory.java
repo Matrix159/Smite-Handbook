6e3e9f04c0643b8d222ec9fd583f35bd6d194c82
Josh Eldridge <jeldridge@michiganlabs.com>
2022-11-27 00:45:14 -0500

    another

--- data/build/generated/source/kapt/debug/com/matrix/data/service/PatchSyncWorker_AssistedFactory.java
+++
@@ -1,8 +0,0 @@
-package com.matrix.data.service;
-
-import androidx.hilt.work.WorkerAssistedFactory;
-import dagger.assisted.AssistedFactory;
-
-@AssistedFactory
-public interface PatchSyncWorker_AssistedFactory extends WorkerAssistedFactory<PatchSyncWorker> {
-}
